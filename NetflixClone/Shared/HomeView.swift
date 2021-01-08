5e0724cb7cd7d3c2a65393c98d5ba0061a89b45b
Javier Galera <javier.galera@bestsecret.com>
2021-01-08 12:29:05 +0100

    Closed #3 Adding kingfisher to request image quickly

---
+++ NetflixClone/Shared/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  NetflixClone
+//
+//  Created by Galera, Javier on 08/01/2021.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Welcome to Netflix")
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
