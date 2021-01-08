3bdf35ce22ef1a9f59729de2791e64f9f1bfb326
Javier Galera <javier.galera@bestsecret.com>
2021-01-08 11:53:27 +0100

    Closed #1 Initializing project

---
+++ NetflixClone/NetflixClone/NetflixCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetflixCloneApp.swift
+//  NetflixClone
+//
+//  Created by Galera, Javier on 08/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct NetflixCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
