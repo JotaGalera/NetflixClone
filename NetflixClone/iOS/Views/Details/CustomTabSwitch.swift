49750e53ab61a14c8b0ac1a5dc63b9b020cc9a29
Jota Galera <javigalera94@gmail.com>
2021-01-15 16:17:04 +0100

    Improving Custom Tab Switch design

--- NetflixClone/iOS/Views/Details/CustomTabSwitch.swift
+++ NetflixClone/iOS/Views/Details/CustomTabSwitch.swift
@@ -8,37 +8,58 @@
 import SwiftUI
 
 struct CustomTabSwitch: View {
+    
+    @State private var currentTab: CustomTab = .episodes
+    
     var tabs: [CustomTab] = [.episodes, .trailers, .more]
+    
+    var body: some View {
+        VStack{
+            ScrollView(.horizontal, showsIndicators: false){
+                HStack(spacing: 20) {
+                    ForEach(tabs, id: \.self) { tab in
+                        VStack {
+                            Rectangle()
+                                .frame(width: widthForTab(tab), height: 6)
+                                .foregroundColor(getSelectorForegroundColor(tab: tab))
+                        
+                            Button(action: {
+                                currentTab = tab
+                            }, label: {
+                                Text(tab.rawValue)
+                                    .font(.system(size: 16, weight: .bold))
+                                    .foregroundColor(getFontColor(tab: tab))
+                            })
+                            .buttonStyle(PlainButtonStyle())
+                            .frame(width: widthForTab(tab), height: 30)
+                        }
+                    }
+                }
+            }
+            
+            switch currentTab {
+            case .episodes:
+                Text("EPISODES")
+            case .trailers:
+                Text("TRAILERS")
+            case .more:
+                Text("MORE")
+            }
+        }
+        .foregroundColor(.white)
+    }
     
     func widthForTab(_ tab: CustomTab) -> CGFloat {
         let string = tab.rawValue
         return string.widthOfString(usingFont: .systemFont(ofSize: 16, weight: .bold))
     }
     
-    var body: some View {
-        VStack{
-            ScrollView(.horizontal, showsIndicators: false){
-                
-                HStack {
-                    ForEach(tabs, id: \.self) { tab in
-                        VStack {
-                            Rectangle()
-                                .frame(width: widthForTab(tab), height: 6)
-                        
-                        
-                            Button(action: {
-                                //
-                            }, label: {
-                                Text(tab.rawValue)
-                                    .font(.system(size: 16, weight: .bold))
-                            })
-                            .buttonStyle(PlainButtonStyle())
-                        }
-                    }
-                }
-            }
-        }
-        .foregroundColor(.white)
+    func getSelectorForegroundColor(tab: CustomTab) -> Color {
+        return currentTab == tab ? .red : .clear
+    }
+    
+    func getFontColor(tab: CustomTab) -> Color {
+        return currentTab == tab ? .white : .gray
     }
 }
 
